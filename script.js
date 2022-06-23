f167ee10334b1a6a2f333db0a25abaa8d3ba5b47
Johab Freitas <freitasjohab@gmail.com>
2022-06-23 16:44:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const mario = document.querySelector(".super-mario");
+const pipe = document.querySelector(".pipe-game");
+/**
+ * => arrow function
+ */
+
+const jump = () => {
+    mario.classList.add("jump-mario");
+
+    setTimeout (() => {
+        mario.classList.remove("jump-mario");
+    }, 500);
+};
+
+const loopGame = setInterval(() => {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+})
+
+document.addEventListener("keydown", jump);
